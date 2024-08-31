20bea1bee926581b3edc980912ab6b114bef9b89
ghasem.shirdel <shirdelghasem79@gmail.com>
2024-08-31 14:30:36 +0330

    feat: fetch zipline repeatedly

--- composeApp/src/androidMain/kotlin/com/parsuomash/telescope/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/parsuomash/telescope/MainActivity.kt
@@ -2,12 +2,19 @@ package com.parsuomash.telescope
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import app.cash.zipline.Zipline
+import androidx.lifecycle.lifecycleScope
 import app.cash.zipline.loader.DefaultFreshnessCheckerNotFresh
 import app.cash.zipline.loader.LoadResult
 import app.cash.zipline.loader.ManifestVerifier
@@ -20,27 +27,27 @@ import com.parsuomash.telescope.notifier.NotifierManager
 import com.parsuomash.telescope.notifier.ProvideNotificationConfiguration
 import com.parsuomash.telescope.notifier.extensions.onCreateOrOnNewIntent
 import com.parsuomash.telescope.notifier.permission.notificationPermissionRequester
-import java.util.concurrent.*
-import kotlinx.coroutines.CoroutineDispatcher
+import java.util.concurrent.Executors
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.asCoroutineDispatcher
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidContext
 
 class MainActivity : ActivityScope() {
+    private var game: String by mutableStateOf("")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val executorService = Executors.newFixedThreadPool(1) {
-            Thread(it, "Zipline")
-        }
-        val dispatcher = executorService.asCoroutineDispatcher()
         try {
-            runBlocking(dispatcher) {
-                val zipline = launchZipline(dispatcher)
-                val gameService = getGameService(zipline)
-                Toast.makeText(this@MainActivity, gameService.calculate(), Toast.LENGTH_SHORT).show()
-            }
+            launchZipline()
         } catch (e: Exception) {
             e.printStackTrace()
         }
@@ -63,25 +70,63 @@ class MainActivity : ActivityScope() {
             }
 
             ProvideNotificationConfiguration(notificationConfiguration) {
-                App()
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text(text = game)
+                }
             }
         }
     }
 
-    fun getGameService(zipline: Zipline): GameService {
-        return zipline.take("gameService")
+    fun launchZipline() {
+        val executorService = Executors.newFixedThreadPool(1) {
+            Thread(it, "Zipline")
+        }
+        val dispatcher = executorService.asCoroutineDispatcher()
+
+        lifecycleScope.launch(dispatcher + SupervisorJob()) {
+            val manifestUrl = "http://172.30.230.141:8080/manifest.zipline.json"
+            val loader = ZiplineLoader(
+                dispatcher,
+                ManifestVerifier.NO_SIGNATURE_CHECKS,
+                OkHttpClient(),
+            )
+            val loadResultFlow = loader.load(
+                applicationName = "game",
+                freshnessChecker = DefaultFreshnessCheckerNotFresh,
+                manifestUrlFlow = repeatFlow(manifestUrl, 1.seconds)
+            )
+
+            var previousJob: Job? = null
+
+            loadResultFlow.collect { result ->
+                previousJob?.cancel()
+
+                if (result is LoadResult.Success) {
+                    val zipline = result.zipline
+                    val presenter = zipline.take<GameService>("gameService")
+
+                    val job = launch {
+                        game = presenter.calculate()
+                    }
+
+                    job.invokeOnCompletion {
+                        presenter.close()
+                        zipline.close()
+                    }
+
+                    previousJob = job
+                }
+            }
+        }
     }
 
-    suspend fun launchZipline(dispatcher: CoroutineDispatcher): Zipline {
-        val manifestUrl = "http://localhost:8080/manifest.zipline.json"
-        val loader = ZiplineLoader(
-            dispatcher,
-            ManifestVerifier.NO_SIGNATURE_CHECKS,
-            OkHttpClient(),
-        )
-        return when (val result = loader.loadOnce("game", DefaultFreshnessCheckerNotFresh, manifestUrl)) {
-            is LoadResult.Success -> result.zipline
-            is LoadResult.Failure -> error(result.exception)
+    /** Poll for code updates by emitting the manifest on an interval. */
+    private fun <T> repeatFlow(content: T, delay: Duration): Flow<T> {
+        return flow {
+            while (true) {
+                emit(content)
+                delay(delay)
+            }
         }
     }
 
